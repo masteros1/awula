29e6a9470e02cf0ab9098ed3c7303aaa7d851450
masteros <judeobodaisai@gmail.com>
2024-08-30 09:29:58 +0000

    cart

--- core/urls.py
+++ core/urls.py
@@ -3,14 +3,13 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('', views.store, name='store'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
-
-     path('update_item/', views.checkout, name='update_item'),
-     path('process_order/', views.processOrder, name='process_order'),
-
+    path('update_item/', views.updateItem, name='update_item'),  # Fixed to call views.updateItem
+    path('process_order/', views.processOrder, name='process_order'),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- ec/settings.py
+++ ec/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ec.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':[os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
 ]
+MEDIA_URL = '/media/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
 
 
